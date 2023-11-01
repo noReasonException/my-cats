0595687f0eb256bfa7071fdefaad40eb79ef1769
Stefanos Stefanou <stefanos.stefanou@century.tech>
2023-11-01 13:38:25 +0000

    Proper Validated as ApplicativeError with BiFunctor

--- src/main/scala/mycats/instances/ValidatedInstances.scala
+++ src/main/scala/mycats/instances/ValidatedInstances.scala
@@ -4,18 +4,29 @@ import mycats.lib.morphisms.bi.BiFunctor
 import mycats.lib.morphisms.error.ApplicativeError
 
 object ValidatedInstances {
-  //??? next time
+
   implicit def validatedApplicativeErrorInstance[E,A]=
     new ApplicativeError[({type Valid[C] = Validated[E, C]})#Valid,E] with BiFunctor[Validated]{
       override def raiseError[A](e:  E): Validated[E, A] = Invalid(e)
-      override def handleErrorWith[A](fa:  Validated[E, A])(f:  E => Validated[E, A]): Validated[E, A] = ???
-      override def handleError[A](fa:  Validated[E, A])(f:  E => A): Validated[E, A] = ???
-      override def bimap[A, B, C, D](fab:  Validated[A, B])(fac:  A => C)(fbd:  B => D): Validated[B, D] = ???
+      override def handleErrorWith[A](fa:  Validated[E, A])(f:  E => Validated[E, A]): Validated[E, A] = fa match {
+        case p@Valid(_) => p
+        case Invalid(invalid) => f(invalid)
+      }
+      override def handleError[A](fa:  Validated[E, A])(f:  E => A): Validated[E, A] = handleErrorWith(fa)(f andThen pure)
+
+      override def bimap[A, B, C, D](fab:  Validated[A, B])(fac:  A => C)(fbd:  B => D): Validated[C, D] = fab match {
+        case Valid(valid) => Valid(fbd(valid))
+        case Invalid(invalid) => Invalid(fac(invalid))
+      }
       override def pure[A](a:  A): Validated[E, A] = Valid(a)
       override def map[A, B](fa:  Validated[E, A])(f:  A => B): Validated[E, B] = fa match {
         case Valid(valid) => Valid(f(valid))
         case p@Invalid(invalid)=>p
       }
-      override def ap[A, B](ff:  Validated[E, A => B])(fa:  Validated[E, A]): Validated[E, B] = ???
+      override def ap[A, B](ff:  Validated[E, A => B])(fa:  Validated[E, A]): Validated[E, B] = (ff,fa) match {
+        case (Valid(f),Valid(a))=>Valid(f(a))
+        case (Invalid(err),_)=>Invalid(err)
+        case (_,Invalid(err))=>Invalid(err)
+      }
   }
 }
